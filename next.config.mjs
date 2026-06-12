2d59ea1f3939ec9606c3e420011dc56a83721100
Ammad Sarfraz <asyousufzai@hotmail.com>
2026-06-12 19:28:24 +0500

    feat: add standalone output and public dir for Docker build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        unoptimized: true
-    }
-};
+  output: 'standalone',
+  images: {
+    unoptimized: true
+  }
+}
 
-export default nextConfig;
+export default nextConfig
